6a1ccb187a9fbdebc36059767fbbaf4319180780
unknown <tvot21@gmail.com>
2022-09-14 12:58:31 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string findCube()
+{
+    int cube = 0;
+ 
+    for (int i = 1; i <= n; i++)
+    {
+        cube = i * i * i;
+        Console.Write(cube + ", ");
+    }
+    return  "";
+}
+Console.WriteLine(findCube());
---
+++ task19/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double EuclideanDistance();
+{
+Console.WriteLine("Введите координаты точки A ");
+Console.Write("x:  ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("y:  ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("z:  ");
+int zA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B ");
+Console.Write("x:  ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("y:  ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("z:  ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2) + (Math.Pow(zA-zA, 2)));
+
+double resalt =  distance;
+}
+
+
+Console.WriteLine("Расстояние между точками = " + Math.Round(EuclideanDistance(), 2));
